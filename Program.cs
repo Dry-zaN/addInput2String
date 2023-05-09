4118716b1b6236f6959b85ed715317e2859f5416
Dry-zaN <123409261+Dry-zaN@users.noreply.github.com>
2023-05-09 12:35:59 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,3 @@
-﻿using System;
-
 namespace Application
 {
 
@@ -7,16 +5,30 @@ namespace Application
     {
         static void Main()
         {
+            string navn;
+            int alder;
             //Outputter uden at lave en ny linje, som gør at jeg kan skrive mit navn på samme linje.
             Console.Write("Giv mig dit fornavn: ");
             //Asks you for a text-based input and adds it to the string "navn"
-            string navn = Console.ReadLine();
+            navn = Console.ReadLine();
+            if (navn.Length == 0)
+            {
+                Console.WriteLine("Du har ikke indtastet noget\nProgrammet lukker næste gang du taster.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
             Console.WriteLine();
             Console.Clear();
             //Asks you for your age. \n will make a new line. 
             Console.Write("Hvor gammel er du?\nGiv mig din alder: ");
-            //Asks you for your age and adds it to the string "alder"
-            string alder = Console.ReadLine();
+            //Asks you for your age and converts the input to Int32 before adding it to the int "alder" 
+            alder = Convert.ToInt32(Console.ReadLine());
+            if (alder <0 || alder == 0)
+            {
+                Console.WriteLine("You are wrong my friend.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Hey {0}! Min processor er også {1} gammel! Nice!", navn, alder);
